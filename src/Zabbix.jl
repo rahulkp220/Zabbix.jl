73b1cc4c5b7c5c1b1d5964704f2ca148d083600a
rahul lakhanpal <rahulkp220@gmail.com>
2018-03-09 00:43:06 +0530

    UPDATES: better visual indentation

--- src/Zabbix.jl
+++ src/Zabbix.jl
@@ -1,5 +1,6 @@
 module Zabbix
 
+# Imports
 using Requests
 using JSON
 
@@ -12,19 +13,19 @@ ZabbixAPI("http://SERVER_URL/zabbix/api_jsonrpc.php", "USERNAME", "PASSWORD", 1,
 """
 type ZabbixAPI
 
-# To be supplied fields
-server_url::String
-username::String
-password::String
-id::Int64
+    # To be supplied fields
+    server_url::String
+    username::String
+    password::String
+    id::Int64
 
-# By default set fields
-headers::Dict
-jsonrpc::String
+    # By default set fields
+    headers::Dict
+    jsonrpc::String
 
-# Inner constructor
-ZabbixAPI(server_url,username,password,id=1,headers=Dict("Content-Type"=>"application/json-rpc"),
-jsonrpc="2.0") = new(server_url,username,password,id,headers,jsonrpc)
+    # Inner constructor
+    ZabbixAPI(server_url,username,password,id=1,headers=Dict("Content-Type"=>"application/json-rpc"),
+    jsonrpc="2.0") = new(server_url,username,password,id,headers,jsonrpc)
 
 end
 
@@ -60,16 +61,16 @@ julia> auth_token(z)
 """
 function auth_token(z::ZabbixAPI)
 
-# construct data
-dict_data=Dict("jsonrpc"=>z.jsonrpc,"id"=>z.id,"method"=>"user.login",
-        "params"=>Dict("user"=>z.username,"password"=>z.password))
-json_data = JSON.json(dict_data)
+    # construct data
+    dict_data=Dict("jsonrpc"=>z.jsonrpc,"id"=>z.id,"method"=>"user.login",
+            "params"=>Dict("user"=>z.username,"password"=>z.password))
+    json_data = JSON.json(dict_data)
 
-# requests data from zabbix
-output = Requests.post(z.server_url,data=json_data,headers=z.headers)
+    # requests data from zabbix
+    output = Requests.post(z.server_url,data=json_data,headers=z.headers)
 
-# return token
-JSON.parse(convert(String, output.data))["result"]
+    # return token
+    JSON.parse(convert(String, output.data))["result"]
 
 end
 
@@ -87,19 +88,19 @@ Dict{String,Any} with 3 entries:
 """
 function get_all_hosts(z::ZabbixAPI)
 
-# get token
-token = auth_token(z)
+    # get token
+    token = auth_token(z)
 
-# construct data
-dict_data = Dict("jsonrpc"=>z.jsonrpc,"id"=>z.id,"method"=>"host.get","auth"=>token,
-                    "params"=>Dict("output"=>["hostid","host"],"selectInterfaces"=>["interfaceid","ip"]))
-json_data = JSON.json(dict_data)
+    # construct data
+    dict_data = Dict("jsonrpc"=>z.jsonrpc,"id"=>z.id,"method"=>"host.get","auth"=>token,
+                        "params"=>Dict("output"=>["hostid","host"],"selectInterfaces"=>["interfaceid","ip"]))
+    json_data = JSON.json(dict_data)
 
-# requests data from zabbix
-output = Requests.post(z.server_url,data=json_data,headers=z.headers)
+    # requests data from zabbix
+    output = Requests.post(z.server_url,data=json_data,headers=z.headers)
 
-# return token
-JSON.parse(convert(String, output.data))
+    # return token
+    JSON.parse(convert(String, output.data))
 
 end
 
@@ -110,6 +111,10 @@ Make request to the zabbix server
 
 For references on various methods available in Zabbix,
 head over to https://www.zabbix.com/documentation/2.2/manual/api/reference
+
+julia> Zabbix.make_request(zabbix, "apiinfo.version", Dict())
+"3.2.11"
+
 """
 function make_request(z::ZabbixAPI, method::String, params=Dict())
     if method == "apiinfo.version" && params == Dict()
@@ -133,7 +138,7 @@ function make_request(z::ZabbixAPI, method::String, params=Dict())
     end
 end
 
-
+# Exports
 export ZabbixAPI, api_version, get_all_hosts, make_request
 
 end
