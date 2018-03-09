df4dbbf91bc47d67c2b7e40c1de8c7a98f5e6e0f
Rahul Lakhanpal <rahulkp220@users.noreply.github.com>
2018-03-09 13:27:51 +0530

    Update Zabbix.jl

--- src/Zabbix.jl
+++ src/Zabbix.jl
@@ -9,7 +9,8 @@ using JSON
 """
 # Zabbix API
 
-#create the zabbix object
+#### create the zabbix object
+
 julia>z = ZabbixAPI("http://SERVER_URL/zabbix/api_jsonrpc.php","USERNAME","PASSWORD")
 ZabbixAPI("http://SERVER_URL/zabbix/api_jsonrpc.php", "USERNAME", "PASSWORD", 1, Dict("Content-Type"=>"application/json-rpc"), "2.0")
 
@@ -36,10 +37,13 @@ end
 """
 # Gets the apiinfo.version data from zabbix server
 
-#create the zabbix object
+#### create the zabbix object
+
 julia>z = ZabbixAPI("http://SERVER_URL/zabbix/api_jsonrpc.php","USERNAME","PASSWORD")
 
-#get the api version 
+
+#### get the api version
+
 julia>api_version(z)
 
 "3.2.11"
@@ -63,10 +67,13 @@ end
 """
 # Gets the authentication token from zabbix server
 
-#create the zabbix object
+#### create the zabbix object
+
 julia>z = ZabbixAPI("http://SERVER_URL/zabbix/api_jsonrpc.php","USERNAME","PASSWORD")
 
-#get the auth token
+
+#### get the auth token
+
 julia> auth_token(z)
 
 "bc86891a1c6c9ef5d41d640eb258a81a"
@@ -92,10 +99,13 @@ end
 """
 # Gets all host for a user
 
-#create the zabbix object
+#### create the zabbix object
+
 julia>z = ZabbixAPI("http://SERVER_URL/zabbix/api_jsonrpc.php","USERNAME","PASSWORD")
 
-#get all hosts for a user
+
+#### get all hosts for a user
+
 julia>get_all_hosts(z)
 
 Dict{String,Any} with 3 entries:
@@ -131,35 +141,35 @@ The make_request function requires you to pass methods(aka Zabbix methods like h
 For references on various methods available in Zabbix,
 head over to https://www.zabbix.com/documentation/2.2/manual/api/reference
 
+#### create the zabbix object
 
-## Another way to get the zabbix version
-
-#create the zabbix object
 julia>z = ZabbixAPI("http://SERVER_URL/zabbix/api_jsonrpc.php","USERNAME","PASSWORD")
 
 julia> Zabbix.make_request(z, "apiinfo.version", Dict())
 
 "3.2.11"
 
+#### create the zabbix method
 
-## Getting the details of a host given its hostname
-
-#create the zabbix method
 julia> method = "host.get"
 
 "host.get"
 
-#construct the params dict object
+
+#### construct the params dict object
+
 julia> params = Dict("output"=>"extend", "filter"=>Dict("host"=>["localhost"]))
 
 Dict{String,Any} with 2 entries:
   "output" => "extend"
   "filter" => Dict("host"=>String["localhost"])
 
-#create the zabbix object
+#### create the zabbix object
+
 julia>z = ZabbixAPI("http://SERVER_URL/zabbix/api_jsonrpc.php","USERNAME","PASSWORD")
 
-#finally make request
+#### finally make request
+
 julia> Zabbix.make_request(z, method, params)
 
 Dict{String,Any} with 3 entries:
@@ -167,7 +177,8 @@ Dict{String,Any} with 3 entries:
   "jsonrpc" => "2.0"
   "result"  => Any[Dict{String,Any}(Pair{String,Any}("lastaccess", "0"),Pair{String,Any}("ipmi_privilege", "2"),Pair{String,Any}("ipmi_error…
 
-#display all entries for a host
+#### display all entries for a host
+
 julia> Zabbix.make_request(z, method, params)["result"][1]
 
 Dict{String,Any} with 39 entries:
@@ -206,7 +217,8 @@ Dict{String,Any} with 39 entries:
   ⋮                    => ⋮
 
 
-#get the host id of the zabbix host
+#### get the host id of the zabbix host
+
 julia> Zabbix.make_request(z, method, params)["result"][1]["hostid"]
 
 "10084"
